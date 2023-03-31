65e840b1722ef8954700998906c3bc0daa5e0a9e
Alisson Cavalcante <alissonsilva0255@gmail.com>
2023-03-31 02:47:56 -0300

    First commit

---
+++ src/br/com/dio/programanointellij.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class programanointellij {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("Hello, World! " + (a+b));
+    }
+}
